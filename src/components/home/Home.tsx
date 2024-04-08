1811885dfd5df1b86a656f473eecff31cc4b8927
nirajlama <nlama@hamropatro.com>
2024-04-08 16:18:25 +0545

    initial commit

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className="w-full border border-white">
+      <div className="w-full grid grid-cols-2 items-center bg-gray-900">
+        <div className="bg-gray-600 aspect-video grid place-items-center">
+          <div className="p-2 bg-blue-400 rounded-full">K</div>
+        </div>
+        <div className="bg-gray-600 aspect-video grid place-items-center">
+          <div className="p-2 bg-blue-400 rounded-full">N</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
